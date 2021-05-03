916e1e4e82a45ab947313d7866e2cefff327f627
Liwer <p.liwerski@gmail.com>
2021-05-03 17:32:34 +0200

    add static settigns

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -36,12 +37,16 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'config.urls'
 
 # --- STATIC FILES ---
-STATIC_ROOT = "/static/"
-STATICFILES_FINDERS = (
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-)
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_ROOT = "/media/"
 
 TEMPLATES = [
